192bfe628d34b3a5c8ffea3bc314cbec82a83f9e
jose antonio <jsegmel@gmail.com>
2022-12-12 17:48:46 +0100

    asincronia

--- guardarImagen.js
+++ guardarImagen.js
@@ -1,7 +1,30 @@
 import { GALERIA } from "./const";
 
-form.addEventListener('click',(e)=>{
-    let foto=document.getElementById('inputPhoto').value
-    GALERIA.unshift(foto)
-    console.log(GALERIA.map)
+const toBase64 = ( file ) => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = error => reject(error);
+});
+
+form.addEventListener('',(e)=>{
+    let foto=document.getElementById('inputPhoto').target
+   
+    console.log(foto)   
+}) 
+
+//document.getElementById('inputPhoto').addEventListener('change',async (val)=> {
+//    const foto = await toBase64(val.target.files[0])
+//    document.getElementById("containerPhoto").src=foto
+//})
+
+
+document.getElementById('inputPhoto').addEventListener('change', (val)=> {
+    const file = val.target.files[0]
+    toBase64(file)
+    .then(val => {
+        document.getElementById("containerPhoto").src=val
+    })
+    .catch(error => console.error(error))
+    
 })
